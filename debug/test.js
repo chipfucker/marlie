34bd1bfdc3bf865765f73893457edba23ad60362
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-09-16 18:05:09 +0000

    move files to debug directory

--- debug/test.js
+++ debug/test.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const { config } = require("./config.json");
-const { post } = require("./utility/rule34api.js");
+const { config } = require("../config.json");
+const { post } = require("../utility/rule34api.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once("clientReady", async () => {
