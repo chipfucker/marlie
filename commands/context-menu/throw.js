d1ff2a34e12c5260ba2006801f1b34762040853f
Marlie <lizzpegschris@gmail.com>
2025-09-25 14:32:25 -0500

    simplify throw command

--- commands/context-menu/throw.js
+++ commands/context-menu/throw.js
@@ -16,67 +16,20 @@ module.exports = {
 
 		// set ./catch/message path
 		const message = i.targetMessage;
-		const msgPath = path.join("catch", i.id);
+		const catchPath = path.join("./catch");
+		if (!fs.existsSync(catchPath)) fs.mkdirSync(catchPath);
 
-		// create message folder
-		fs.mkdirSync(msgPath, (err) => { throw err; });
+		const file = path.join(catchPath, i.id + ".json");
 
-		// set quote variables
-		let content;
-		const dataDisplay = [];
+		fs.writeFileSync(file, JSON.stringify(i.targetMessage, null, "\t"));
 
-		// CREATE FILES
-		if (message.content) {
-			fs.writeFileSync(path.join(msgPath, "content.txt"), message.content);
-			content = (message.content.length > 128)
-				? message.content.substring(0, 128) + "`\u2022 \u2022 \u2022`"
-				: message.content;
-		}
-		if (message.attachments.size) {
-			const filesPath = path.join(msgPath, "attachments");
-			fs.mkdirSync(filesPath);
-			for (const [key, value] of message.attachments) {
-				const attachment = await fetch(value.url)
-					.then(e => e.arrayBuffer())
-					.then(e => Buffer.from(e));
-				fs.writeFileSync(
-					path.join(filesPath, `${value.id}~${value.name}`),
-					attachment
-				)
-			}
-			dataDisplay.push(`${message.attachments.size} attachment${(message.attachments.size !== 1) ? "s" : ""}`);
-		}
-		if (message.embeds.length) {
-			const embedsPath = path.join(msgPath, "embeds");
-			fs.mkdirSync(embedsPath);
-			for (const embed of message.embeds) {
-				fs.writeFileSync(
-					path.join(embedsPath, `${embed.data.id}.json`),
-					JSON.stringify(embed, null, "\t")
-				);
-			}
-			dataDisplay.push(`${message.embeds.length} embed${(message.embeds.length !== 1) ? "s" : ""}`);
-		}
-
-		// set quote
-		const data = (() => {
-			if (dataDisplay.length) return "```\n"+dataDisplay.join("\n")+"\n```";
-			else return null;
-		})();
-		const quote = [content, data].join("\n\n").trim();
-
-		console.log(
-			`\n######  Someone threw you a message! I wonder who...\n##\n##  ${
-				quote.replace(/\n/g, "\n##  ")
-			}\n##\n######  Saved to file://catch/${i.id}/`
-		);
-
+		console.log(`    MESSAGE THROWN: ${file}`)
 		if (i.channel.id === "1384093405017018399") {
-			await i.editReply(`Message thrown!\n>>> ${quote}`);
+			await i.editReply(`Message thrown!\n\`${file}\``);
 		}
 		else {
 			const channel = await i.client.channels.fetch("1384093405017018399");
-			const response = await channel.send(`Message thrown from <#${i.channel.id}>!\n>>> ${quote}`);
+			const response = await channel.send(`Message thrown from <#${i.channel.id}>!\n\`${file}\``);
 			await i.editReply(`Message thrown! https://discord.com/channels/1294885290275770430/1384093405017018399/${response.id}`);
 		}
 	}
