3fb1dd0cecc6eddc323ac4c8dace45dd84a35123
betterboruu <nightofthebuttlickers@gmail.com>
2025-09-29 10:57:41 -0500

    generic save

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const Discord = require("discord.js");
-const { config } = require("./secrets.json");
+import * as fs from "node:fs";
+import path from "node:path";
+import * as Discord from "discord.js";
+import { config } from "./secrets.json" with { type: "json" };
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
@@ -9,31 +9,31 @@ client.commands = new Discord.Collection();
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
+for (const folder of commandFolders) (async () => {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
+		const command = await import(filePath);
 		if ("data" in command && "execute" in command) {
 			client.commands.set(command.data.name, command);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
-}
+})();
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
-for (const file of eventFiles) {
+for (const file of eventFiles) (async () => {
 	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
+	const event = await import(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
-}
+})();
 
 client.login(config.token);
--- refresh.js
+++ refresh.js
@@ -1,8 +1,8 @@
-const Discord = require("discord.js");
-const { config } = require("./secrets.json");
-const fs = require("node:fs");
-const path = require("node:path");
-const wait = require("node:timers/promises").setTimeout;
+import * as Discord from "discord.js";
+import secrets from "./secrets.json";
+import * as fs from "node:fs";
+import path from "node:path";
+import { setTimeout as wait } from "node:timers/promises";
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
@@ -10,36 +10,36 @@ const commands = [];
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
+for (const folder of commandFolders) (async () => {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
+		const command = await import(filePath);
 		if ("data" in command && "execute" in command) {
 			commands.push(command.data.toJSON());
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
-}
+})();
 
-const rest = new Discord.REST().setToken(config.token);
+const rest = new Discord.REST().setToken(secrets.config.token);
 
 (async () => {
 	console.log("\x1b[91m\x1b[1mRFS\x1b[0m deleting commands")
-	await rest.put(Discord.Routes.applicationCommands(config.clientId), { body: [] })
+	await rest.put(Discord.Routes.applicationCommands(secrets.config.clientId), { body: [] })
 		.then(() => console.log("  deleted application commands"))
 		.catch(console.error);
-	await rest.put(Discord.Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
+	await rest.put(Discord.Routes.applicationGuildCommands(secrets.config.clientId, secrets.config.guildId), { body: [] })
 		.then(() => console.log("  deleted guild commands"))
 		.catch(console.error);
 	console.log(`\x1b[91m\x1b[1mRFS\x1b[0m refreshing ${commands.length} commands`);
 	await wait(500).then(() => console.log("  .5 seconds elapsed")).catch(console.error);
-	await rest.put(Discord.Routes.applicationCommands(config.clientId), { body: commands })
+	await rest.put(Discord.Routes.applicationCommands(secrets.config.clientId), { body: commands })
 		.then(e => console.log(`  refreshed ${e.length} commands`))
 		.catch(console.error);
-	await client.login(config.token);
+	await client.login(secrets.config.token);
 })();
 	
 client.once("clientReady", async (client) => {
