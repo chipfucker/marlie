c3512660258b58856c30d01a3bc9576ac891b8e0
betterboruu <nightofthebuttlickers@gmail.com>
2025-11-06 16:49:13 -0600

    add to secret.js

--- secret.js
+++ secret.js
@@ -1,16 +1,42 @@
 import secret from "#/secret.json" with { type: "json" };
 
+export const userAgent = secret.UserAgent;
+
 export const bot = {
     token: secret.discord.token,
+    clientId: String(secret.discord.app_id),
     guildId: String(secret.discord.admin_server)
+};
+
+export const user = {
+    main: String(secret.discord.user.main)
 };
 
 export const channel = {
     testing: { id: String(secret.discord.channel.testing) },
-    forum: {
-        id: String(secret.discord.channel.forum),
-        tags: {
-
+    announcements: { id: String(secret.discord.channel.announcements) },
+    main: { id: String(secret.discord.channel.main) },
+    nsfw: { id: String(secret.discord.channel.nsfw) },
+    knit: {
+        id: String(secret.discord.channel.knit),
+        tag: {
+            test: String(secret.discord.channel.knit_tags.TEST),
+            sampling: String(secret.discord.channel.knit_tags.Sampling),
+            goated: String(secret.discord.channel.knit_tags.Goated),
+            bad: String(secret.discord.channel.knit_tags.Bad),
+            exhausted: String(secret.discord.channel.knit_tags.Exhausted),
+            artist: String(secret.discord.channel.knit_tags.Artist),
+            character: String(secret.discord.channel.knit_tags.Character),
+            general: String(secret.discord.channel.knit_tags.General)
+        }
+    },
+    hunting: {
+        id: String(secret.discord.channel.hunt_forum),
+        tag: {
+            found: String(secret.discord.channel.hunt_forum_tags.Found),
+            hunting: String(secret.discord.channel.hunt_forum_tags.Hunting),
+            lost: String(secret.discord.channel.hunt_forum_tags.Lost),
+            marlie: String(secret.discord.channel.hunt_forum_tags.Marlie)
         }
     },
     save: { id: String(secret.discord.channel.save) },
@@ -20,6 +46,13 @@ export const channel = {
     hold: { id: String(secret.discord.channel.hold) }
 };
 
-export const user = {
-    main: String(secret.discord.user.main)
+export const rule34 = {
+    api_key: secret.rule34.api_key,
+    user_id: String(secret.rule34.user_id),
+    // TODO: check if pass_hash has an underscore
+    pass_hash: secret.rule34.pass_hash
 };
+
+const full = Object.assign({ user, channel }, bot);
+
+export default full;
