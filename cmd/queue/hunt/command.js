ed2486b3b255caa3145a503ec57d4d291ee19833
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-11-07 12:35:31 -0600

    complete main queue hunt function

--- cmd/queue/hunt/command.js
+++ cmd/queue/hunt/command.js
@@ -2,7 +2,8 @@ import * as Discord from "discord.js";
 import { channel as secret } from "#/secret.js";
 
 export const data = {
-    name: "Queue for hunting",
+    ready: false,
+    name: "queue-hunt-m",
     type: Discord.ApplicationCommandType.Message,
     integration_types: [ Discord.ApplicationIntegrationType.UserInstall ],
     contexts: [
@@ -11,6 +12,43 @@ export const data = {
     ]
 };
 
-export async function application(i) {
+export async function interaction(i) {
+    const defer = i.deferReply({ flags: [
+        Discord.MessageFlags.Ephemeral,
+        Discord.MessageFlags.IsComponentsV2,
+        Discord.MessageFlags.SuppressNotifications
+    ]});
+
+    const files = [];
+
+    const urls = {
+        attachments: i.targetMessage.attachments.values().map(item => item.url),
+        stickers: i.targetMessage.stickers.values()
+            .filter(item => item.format !== Discord.StickerFormatType.Lottie)
+            .map(item => item.url)
+    };
     
+    files.concat(...Object.values(urls));
+
+    if (!files.length) {
+        await defer.then(() => i.editReply({ components: [
+            { type: Discord.ComponentType.TextDisplay,
+                content: "No images to send."
+            }
+        ]}));
+        return;
+    }
+
+    const channel = await i.client.channels.fetch(secret.hunt.id);
+    const messages = [];
+    for (const index in Array(Math.ceil(files.length / 10))) {
+        messages.push(channel.send({ files: files.slice(index, index + 10) }));
+    }
+
+    await Promise.all(messages);
+    await i.editReply({ components: [
+        { type: Discord.ComponentType.TextDisplay,
+            content: `Sent ${files.length} file${files.length === 1 ? "" : "s"} to ${channel.url}!`
+        }
+    ]});
 }
