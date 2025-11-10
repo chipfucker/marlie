ae635f32b328a77f355fa15403a18c6b6fdcd866
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-11-10 12:40:17 -0600

    update queue-save

--- cmd/queue/save/command.js
+++ cmd/queue/save/command.js
@@ -40,7 +40,7 @@ export async function ChatInputCommandInteraction(i) {
     }
 
     const channel = await i.client.channels.fetch(secret.save.id);
-    await execute(channel, links);
+    await execute(channel, links.join("\n"));
 
     await defer.then(() => i.editReply({
         content: `Sent ${links.length} link${links.length===1?"":"s"} to ${channel.url}!`
@@ -49,20 +49,78 @@ export async function ChatInputCommandInteraction(i) {
 
 export async function MessageContextMenuCommandInteraction(i) {
     const defer = i.reply({
-        flags: [
-            Discord.MessageFlags.Ephemeral,
-            Discord.MessageFlags.SuppressNotifications
-        ],
+        flags: [ Discord.MessageFlags.Ephemeral ],
         content: "Getting URLs..."
     });
 
-    /* possible regex:
-        /Tags: .*?\n(?:\n.*\[Source\]\(<(.*?)>\))+(?:\n\n.*)/g
-    */
+    const links = (() => {
+        // TODO: verify user tag property works here
+        if (i.targetMessage.author.tag === "Lawliet#5480")
+            return i.targetMessage.content.match(/\[Source\]\(<(.+?)>\)/g)
+                ?.map(item => item.match(/\[Source\]\(<(.+?)>\)/g)[1]);
+        else
+            return i.targetMessage.content.match(/(?:http|https|ftp):\/\/(?:\S*)/ig);
+    })();
+
+    if (!links) {
+        await defer.then(() => i.editReply({
+            content: "No URLs to send."
+        }));
+        return;
+    }
+
+    const channel = await i.client.channels.fetch(secret.save.id);
+    await execute(channel, links.join("\n"));
+
+    await defer.then(() => i.editReply({
+        content: `Sent ${links.length} link${links.length===1?"":"s"} to ${channel.url}!`
+    }));
+}
+
+export async function messageCreate(m, args) {
+    const defer = m.reply({
+        allowedMentions: { repliedUser: false },
+        content: "Getting URLs..."
+    });
+
+    const channel = await m.client.channels.fetch(secret.save.id);
+
+    if (args) {
+        await execute(channel, args);
+        await defer.then(() => m.editReply({
+            allowedMentions: { repliedUser: false },
+            content: `Sent content to ${channel.url}!`
+        }));
+        return;
+    } else {
+        const message = await m.channel.messages.fetch({ before: m.id, limit: 1, cache: false })
+            .then(collection => collection.values().toArray()[0]);
+        
+        const links = (() => {
+
+            if (message.author.tag === "Lawliet#5480")
+                return message.content.match(/\[Source\]\(<(.+?)>\)/g)
+                    ?.map(item => item.match(/\[Source\]\(<(.+?)>\)/g)[1]);
+            else
+                return message.content.match(/(?:http|https|ftp):\/\/(?:\S*)/ig);
+        })();
+
+        if (!links) {
+            await defer.then(() => m.editReply({
+                content: "No URLs to send."
+            }));
+            return;
+        }
+        
+        await execute(channel, links.join("\n"));
+        await defer.then(() => m.editReply({
+            content: `Sent ${links.length} link${links.length===1?"":"s"} to ${channel.url}!`
+        }));
+    }
 }
 
 async function execute(channel, links) {
     await channel.send({
-        content: links.join("\n")
+        content: links
     });
 }
