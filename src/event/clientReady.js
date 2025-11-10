49f87f89b901833ebe550bb12c8459d02c98261a
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-11-10 16:16:36 -0600

    fix wrong dir

--- src/event/clientReady.js
+++ src/event/clientReady.js
@@ -6,7 +6,7 @@ export const once = true;
 export async function execute(client) {
     console.log(`Logged into ${client.user.tag}`);
 
-    const status = fs.readFileSync("res/profile/status.txt", "utf8");
+    const status = fs.readFileSync("src/res/profile/status.txt", "utf8");
     client.user.setActivity(status, { type: Discord.ActivityType.Custom })
     console.log(`Set status: ${status}`);
 }
