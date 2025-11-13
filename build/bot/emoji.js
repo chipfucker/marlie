1a9c0a92472f478e999b07367471f2c7c173839b
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-11-13 14:45:09 -0600

    create build/emoji

---
+++ build/bot/emoji.js
@@ -0,0 +1,43 @@
+import * as Discord from "discord.js";
+import * as FileSystem from "node:fs/promises";
+import * as Path from "node:path";
+import Sharp from "sharp";
+import { discord as secret } from "#secret";
+
+const client = new Discord.Client();
+
+client.once(Discord.Events.ClientReady, async client => {
+    const manager = client.application.emojis;
+
+    manager.fetch()
+        .then(emojis => Promise.all(emojis.map(emoji => emoji.delete())))
+        .then(promises => console.log(`Deleted ${promises.length} emoji`));
+
+    const json = {};
+    const path = Path.resolve("res/emoji");
+    await FileSystem.readdir(path, { recursive: true })
+    .then(files => Promise.all(files.map(async file => {
+        // TODO: see if Sharp instance methods modify the instance
+        const original = new Sharp(Path.join(path, file));
+        const { height } = await image.metadata();
+        const resize = await original.resize({
+            height: Math.ceil(128 / height) * height,
+            kernel: "nearest"
+        }).toBuffer();
+
+        const { name } = Path.parse(file);
+        if (!name.match(/\.spr$/)) {
+            const emoji = await manager.create({
+                attachment: resize,
+                name: name
+            }).then(emoji => emoji.toString());
+            json[name] = emoji;
+        }
+    }))).then(promises => console.log(`Uploaded ${promises.length} emoji`));
+
+    FileSystem.writeFile("bin/emoji.json", JSON.stringify(json, null, "\t"))
+        .then(() => console.log("file://bin/emoji.json updated"));
+    client.destroy();
+});
+
+client.login(secret.token);
